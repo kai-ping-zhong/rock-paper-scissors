ed432134b856516bb5ad02ffa42ccb79d2c6f4ac
kai-ping-zhong <kai.zhong@outlook.com>
2020-06-06 22:14:42 +1000

    updated js file

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 // Caching the DOM - Like reference points!
 let userScore = 0;
 let computerScore = 0;
-let numberofRounds = 0;
+let numberofRounds = userScore + computerScore;
 const userScore_span = document.getElementById("user-score");
 const computerScore_span = document.getElementById("computer-score");
 const scoreBoard_div = document.querySelector(".scoreboard");
@@ -17,29 +17,46 @@ function getComputerChoice() {
     return choices[randomNumber];
 }
 
+function convertToWord(letter) {
+if (letter === r) return "Rock";
+if (letter === p) return "Paper";
+return "Scissors";
+}
+
 function win(userChoice, computerChoice) {
     userScore++;
     numberofRounds++;
     userScore_span.innerHTML = userScore;
     computerScore_span.innerHTML = computerScore;
-    result_p.innerHTML = userChoice + "beats" + computerChoice + "You win";
-    console.log(computer);
+    const smalluserWord = "user".fontsize(3).sub();
+    const smallcompWord = "comp".fontsize(3).sub();
+    result_p.innerHTML = `${convertToWord(userChoice)}${smalluserWord} beats ${convertToWord(computerChoice)}${smallcompWord}. You win!`;
 }
-function lose() {
-    console.log("User LOSES");
+function lose(userChoice, computerChoice) {
+    computerScore++;
     numberofRounds++;
+    userScore_span.innerHTML = userScore;
+    computerScore_span.innerHTML = computerScore;
+    const smalluserWord = "user".fontsize(3).sub();
+    const smallcompWord = "comp".fontsize(3).sub();
+    result_p.innerHTML = `${convertToWord(userChoice)}${smalluserWord} loses to ${convertToWord(computerChoice)}${smallcompWord}. You lose!`;
 }
-function draw() {
-    console.log("DRAW");
+function draw(userChoice, computerChoice) {
     numberofRounds++;
+    userScore_span.innerHTML = userScore;
+    computerScore_span.innerHTML = computerScore;
+    const smalluserWord = "user".fontsize(3).sub();
+    const smallcompWord = "comp".fontsize(3).sub();
+    result_p.innerHTML = `${convertToWord(userChoice)}${smalluserWord} same as ${convertToWord(computerChoice)}${smallcompWord}. You draw!`;
 }
+
 
 //Main Game Function
 function game(userChoice) {
     const computerChoice = getComputerChoice();
     const name = "testing";
-    console.log("User chooses" + " " + userChoice);
-    console.log("Computer chooses" + " " + computerChoice);
+    console.log(`User chooses ${userChoice}`);
+    console.log(`Computer chooses ${computerChoice}`);
         switch (userChoice + computerChoice) {
             case "rs":    
             case "pr":
@@ -51,7 +68,9 @@ function game(userChoice) {
             case "sr":
                 lose(userChoice, computerChoice);
                 break;
-            default:
+            case "rr":
+            case "pp":
+            case "ss":
                 draw(userChoice, computerChoice);
                 break;
         }
